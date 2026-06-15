c5771f7a0f098e40a7ab81805ed7cb6ae8980dd6
ramyakuruba2007-sys <ramyakuruba2007@gmail.com>
2026-06-15 05:17:45 -0700

    Add files via upload

---
+++ my first project.py
@@ -0,0 +1,15 @@
+print("Welcome to my Profile")
+
+name=input("Enter your name: ")
+course=input("Enter your course: ")
+
+print("\n----MY PROFLE----")
+print("Name:",name)
+print("Course:",course)
+
+print("\nMy skills: ")
+skills=["Python","C++"]
+
+for skill in skills:
+    print("-",skill)
+print("Thank you!")
